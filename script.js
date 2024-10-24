4a262aa3bcff69a5a2ebfc336261593c0d64fceb
unknown <brunomozer05@gmail.com>
2024-10-24 12:18:27 -0300

    Separando os numeros do input

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function validar() {
-    let cpf = document.getElementById("cpf")
+    const cpfInput = document.getElementById('cpf').value
+    const numeros = cpfInput.match(/\d/g)
+    numeros && numeros.length === 11 
+    let [d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11] = numeros
+
+    document.getElementById('res').innerText = `cpf numeros ${d1}, ${d2}, ${d3}, ${d4}, ${d5}, ${d6}, ${d7}, ${d8}, ${d9}, ${d10}, ${d11}`
 
 }
